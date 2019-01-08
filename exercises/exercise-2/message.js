a053fbd95a7c8c63876ee16a10d8cd0cce27bed3
Mike Spiegel <mspiegel31@gmail.com>
2019-01-08 13:54:47 -0500

    implemented exercise-2

--- exercises/exercise-2/message.js
+++ exercises/exercise-2/message.js
@@ -3,9 +3,26 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default class Message extends React.Component {
   render() {
+
+    const secretMessage = this.props.isASecret && "secret"
+    const message = this.props.isASecret && "hello!"
+
     return (
-      <View>
+      <View style={styles.container}>
+        <Text style={styles.message}>Here's a {secretMessage} message for you: {message}</Text>
       </View>
     );
   }  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    opacity: .9,
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5
+  },
+  message: {
+    color: 'green',
+  },
+});
